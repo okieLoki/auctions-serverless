c118acc34a12102518afcf6952b4afd1b778c132
Uddeepta Raaj Kashyap <uddeeptaraajkashyap@gmail.com>
2023-07-28 10:48:20 +0530

    initial commit

---
+++ auction-service/src/handlers/createAuction.js
@@ -0,0 +1,18 @@
+const createAuction = async (event) => {
+
+  const { title } = JSON.parse(event.body)
+  const now = new Date();
+
+  const auction = {
+    title,
+    status: 'OPEN',
+    createdAt: now.toISOString(),
+  }
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  };
+};
+
+module.exports.handler = createAuction;
