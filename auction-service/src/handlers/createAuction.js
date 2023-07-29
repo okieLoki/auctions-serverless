9637be3b369fcb444adf3275eeeb0e24688f7549
Uddeepta Raaj Kashyap <uddeeptaraajkashyap@gmail.com>
2023-07-29 13:07:34 +0530

    createAuction added

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,13 +1,24 @@
+const { v4: uuid} = require('uuid')
+const AWS = require('aws-sdk')
+
+const dynamodb = new AWS.DynamoDB.DocumentClient()
+
 const createAuction = async (event) => {
 
   const { title } = JSON.parse(event.body)
   const now = new Date();
 
   const auction = {
+    id: uuid(),
     title,
     status: 'OPEN',
     createdAt: now.toISOString(),
   }
+
+  await dynamodb.put({
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Item: auction,
+  }).promise()
 
   return {
     statusCode: 201,
