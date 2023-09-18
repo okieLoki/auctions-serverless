472558d07f30adeb79eeecb691d20a553e8f9330
loki <uddeeptaraajkashyap@gmail.com>
2023-09-18 23:06:45 +0530

    sqs added

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -4,19 +4,24 @@ const ses = new AWS.SES({ region: 'us-east-1' })
 
 const sendMail = async (event) => {
 
+    const record = event.Record[0];
+    
+    const email = JSON.parse(record.body)
+    const {subject, body, recipient} = email
+
     const params = {
         Source: 'testuddeepta@gmail.com',
         Destination: {
-            ToAddresses: ['uddeepta12@gmail.com']
+            ToAddresses: [recipient]
         },
         Message: {
             Body: {
                 Text: {
-                    Data: 'Hello from AWS SES'
+                    Data: body
                 }
             },
             Subject: {
-                Data: 'Hello from AWS SES'
+                Data: subject
             }
         }
     }
