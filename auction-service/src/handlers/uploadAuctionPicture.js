5fc998119bcc538fbb310f7e9893e3db0d0c2706
loki <uddeeptaraajkashyap@gmail.com>
2023-09-22 00:03:37 +0530

    uploadPicture updated

--- auction-service/src/handlers/uploadAuctionPicture.js
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -1,4 +1,12 @@
+const {getAuctionById} = require('./getAuction');
+
 const uploadAuctionPicture = async (event) => {
+
+    const { id } = event.pathParameters;
+    const auction = await getAuctionById(id);
+    const b64 = event.body.replace(/^data:image\/\w+;base64,/, "")
+    const buffer = Buffer.from(b64, 'base64');
+
     return {
         statusCode: 200,
         body: JSON.stringify({
